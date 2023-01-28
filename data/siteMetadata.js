56390f20c16a037da1e8a5b2952622a0a97d8b0d
satoooh <bighope.lumiere0916@gmail.com>
2023-01-29 01:34:44 +0900

    add discord server link

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -18,6 +18,7 @@ const siteMetadata = {
   twitter: 'https://twitter.com/Twitter/9Satoooh',
   facebook: 'https://www.facebook.com/taiki.sato.3785',
   youtube: 'https://youtube.com/@satoooh',
+  discord: 'https://discord.gg/6XvAnymyHY',
   locale: 'ja-JP',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
