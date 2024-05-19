c4482c8aea830e82a7dd2026035c3b6f43138540
satoooh <bighope.lumiere0916@gmail.com>
2024-05-19 23:58:48 +0900

    use icon

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -16,8 +16,10 @@ export default function Datetime({
   size = "sm",
   className,
 }: Props) {
+  const isUpdated = modDatetime && modDatetime > pubDatetime;
+
   return (
-    <div className={`flex items-center space-x-2 opacity-80 ${className}`}>
+    <div className={`flex items-center space-x-1 opacity-80 ${className}`}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className={`${
@@ -28,19 +30,36 @@ export default function Datetime({
         <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
         <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
       </svg>
-      {modDatetime && modDatetime > pubDatetime ? (
-        <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-          Updated:
-        </span>
+      {isUpdated ? (
+        <>
+          <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
+            <FormattedDatetime pubDatetime={pubDatetime} />
+          </span>
+          <span
+            className={`italic ${size === "sm" ? "text-sm" : "text-base"} pl-2`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`${
+                size === "sm" ? "scale-90" : "scale-100"
+              } inline-block h-6 w-6 min-w-[1.375rem] fill-transparent opacity-90 stroke-current stroke-2`}
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
+              <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
+            </svg>
+          </span>
+          <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
+            <FormattedDatetime pubDatetime={modDatetime} />
+          </span>
+        </>
       ) : (
-        <span className="sr-only">Published:</span>
+        <>
+          <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
+            <FormattedDatetime pubDatetime={pubDatetime} />
+          </span>
+        </>
       )}
-      <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-        <FormattedDatetime
-          pubDatetime={pubDatetime}
-          modDatetime={modDatetime}
-        />
-      </span>
     </div>
   );
 }
@@ -56,17 +75,9 @@ const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
     day: "numeric",
   });
 
-  const time = myDatetime.toLocaleTimeString(LOCALE.langTag, {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-
   return (
     <>
       <time dateTime={myDatetime.toISOString()}>{date}</time>
-      <span aria-hidden="true"> | </span>
-      <span className="sr-only">&nbsp;at&nbsp;</span>
-      <span className="text-nowrap">{time}</span>
     </>
   );
 };
